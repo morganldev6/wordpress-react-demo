5983ab86621b6bf025aa17db8300cf527684733d
morganldev6 <morganl@dev6.com>
2019-09-26 16:52:50 -0400

    Not sure why my console logs aren't even running

--- src/App.js
+++ src/App.js
@@ -62,7 +62,23 @@ const Footer = () => {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { movies: [] };
+  }
+
+  fetchPostData() {
+    fetch(`http://localhost/wordpress-demo/wp-json/wp/v2/movies?per_page=100`)
+      .then(response => response.json())
+      .then(myJSON => {
+        // Logic goes here
+        console.log(myJSON);
+        console.log("it worked");
+        this.setState({ movies: myJSON });
+      });
+  }
+
+  componentDidMount() {
+    console.log("component mounted");
+    this.fetchPostData();
   }
 
   render() {
